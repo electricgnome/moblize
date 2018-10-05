2df16afdbb7e21e56f545a1de4f8ff04b2d8f5e8
Hector Saldana <hector@electricgnome.com>
2018-10-05 09:27:32 -0500

    front/back ends working, need to add API calls

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 require("dotenv").config();
 
 const express = require("express");
-const body_parser = require("body-parser");
+const bodyParser = require("body-parser");
 const Promise = require("bluebird");
 const mongoose = require("mongoose");
 const session = require("express-session");
-(redis = require("redis")),
-  (client = redis.createClient(process.env.REDIS_URL || null));
-const routes = require("./routes/index");
+const RedisStore = require("connect-redis")(session);
+const serveStatic = require("serve-static");
 
+const redis = require("redis");
+const client = redis.createClient(process.env.REDIS_URL || null);
+
+// const routes = require("./routes/index");
 const users = require("./routes/users");
+
+const path = require("path");
+const app = express();
+const http = require("http").Server(app);
 
 mongoose.Promise = global.Promise;
 mongoose
@@ -17,19 +24,10 @@ mongoose
   .then(() => console.log("Connection succesful"))
   .catch(err => console.error(err));
 
-const RedisStore = require("connect-redis")(session);
-const serveStatic = require("serve-static");
-const path = require("path");
-const app = express();
-const http = require("http").Server(app);
-
 app.use(express.json());
 
-// app.set("views", path.join(__dirname, "dist"));
-// app.set("view engine", "ejs");
-
-app.use(body_parser.json());
-app.use(body_parser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static("dist"));
 
 const hour = 3600000;
@@ -43,32 +41,10 @@ app.use(
   })
 );
 
-app.use("/", serveStatic(path.join(__dirname, "/dist/moblize"))); //static for VUE
+app.use("/", serveStatic(path.join(__dirname, "/dist/moblize"))); 
 
-// app.use("/", routes);
 app.use("/users", users);
 
-// app.use(function(req, res, next) {
-//   var err = new Error("Not Found");
-//   err.status = 404;
-//   next(err);
-// });
-
-// if (app.get("env") === "development") {
-//   app.use(function(err, req, res, next) {
-//     res.status(err.status || 500);
-//     res.render("error", {
-//       message: err.message,
-//       error: err
-//     });
-//   });
-// }
-
-// app.get("/", function(request, response) {
-//  response.render("index.html");
-//});
-
-//################################----------###############################
 
 const PORT = process.env.PORT || 8801;
 http.listen(PORT, function() {
